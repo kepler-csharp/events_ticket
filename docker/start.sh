e764df4bad7319af3178ca779f41f6dcfc220929
JeritoBa <jerogallego099@gmail.com>
2026-05-27 17:41:28 -0500

    fix: writing enviroment vars of docker printenv into .env

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,11 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
+# Sobrescribir variables del contenedor
+sed -i "s|^APP_ENV=.*|APP_ENV=${APP_ENV}|" .env
+sed -i "s|^APP_KEY=.*|APP_KEY=${APP_KEY}|" .env
+sed -i "s|^APP_URL=.*|APP_URL=${APP_URL}|" .env
+
 php artisan optimize:clear || true
 php artisan config:cache || true
 php artisan view:cache || true
